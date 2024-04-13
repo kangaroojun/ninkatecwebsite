426f71de87ff8951b1130abcbceeb87ddbc484e2
kangaroojun <147029457+kangaroojun@users.noreply.github.com>
2024-04-13 21:46:45 +0800

    Update ninkatecwebsite.py

--- app/ninkatecwebsite.py
+++ app/ninkatecwebsite.py
@@ -248,10 +248,9 @@ if page == 'Add New Syringe':
             st.success("Syringe data added successfully")
 
     with existing_patient:    
-        collection_ref = firestore_db.collection("syringes")
-        for doc in collection_ref.stream():
-            if doc.get('patient_name') != None:
-                patients_sn_list.append([doc.get('patient_name'), doc.get('syringe_sn')])
+        collection_ref = firestore_db.collection("syringes").where('patient_name', '>', '').get()
+        for doc in collection_ref:
+            patients_sn_list.append([doc.get('patient_name'), doc.get('syringe_sn')])
         for patient in patients_sn_list:
             patients_list.append(patient[0])
         selected_patient = st.selectbox('Patient', patients_list)
